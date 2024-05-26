53eed0f5ba46ad86801e11b9a30281c0b47f137d
Abe Sharp <abe@hpe.com>
2024-05-26 10:25:59 -0700

    Update email-local.py

--- email-local.py
+++ email-local.py
@@ -3,13 +3,29 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from smtplib import SMTP
-import datetime
 
 def print_hello():
     return 'Hello world!'
 
 def my_email_func():
-    return 'sent an email!'
+    smtp = SMTP()
+    smtp.set_debuglevel(10)
+    smtp.connect('smtp.sparkpostmail.com', 587)
+    smtp.login('SMTP_Injection', '425ff0c1a2b58a735ae43e821e0a2f36eaa6a50f')
+
+    from_addr = "AlertPlane <info@alertplane.io>"
+    to_addr = "abe24seven@gmail.com"
+
+    subj = "hello"
+    date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+
+    message_text = "Hello\nThis is a mail from your server\n\nBye\n"
+
+    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, date, message_text )
+
+    smtp.sendmail(from_addr, to_addr, msg)
+    smtp.quit()
+    return 'Email sent!'
 
 default_args = {
         'owner': 'abe',

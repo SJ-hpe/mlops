5b30601a664b3d482293fff120d3ab43b326e9cf
Abe Sharp <abe@hpe.com>
2024-05-26 10:24:12 -0700

    Update email-local.py

--- email-local.py
+++ email-local.py
@@ -8,35 +8,18 @@ import datetime
 def print_hello():
     return 'Hello world!'
 
+def my_email_func():
+    return 'sent an email!'
+
 default_args = {
         'owner': 'abe',
         'start_date':datetime(2024,5,26)
 }
 
-def my_email_func():
-    smtp = SMTP()
-    smtp.set_debuglevel(10)
-    smtp.connect('smtp.sparkpostmail.com', 587)
-    smtp.login('SMTP_Injection', '425ff0c1a2b58a735ae43e821e0a2f36eaa6a50f')
-
-    from_addr = "AlertPlane <info@alertplane.io>"
-    to_addr = "abe24seven@gmail.com"
-
-    subj = "hello"
-    date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
-
-    message_text = "Hello\nThis is a mail from your server\n\nBye\n"
-
-    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, date, message_text )
-
-    smtp.sendmail(from_addr, to_addr, msg)
-    smtp.quit()
-    return 'Email sent!'
-
-
 dag = DAG('send_email_test', description='SMTP Function DAG',
           schedule_interval='* * * * *',
           default_args = default_args, catchup=False)
+
 
 dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 

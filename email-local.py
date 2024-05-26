7098b0e4b9f8ec03230e093e68ffcae034ca0b73
Abe Sharp <abe@hpe.com>
2024-05-26 10:19:45 -0700

    Update email-local.py

--- email-local.py
+++ email-local.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.email_operator import EmailOperator
 from smtplib import SMTP
 import datetime
 
@@ -14,7 +13,7 @@ default_args = {
         'start_date':datetime(2024,5,26)
 }
 
-def send_smtp_mail():
+def my_email_func():
     smtp = SMTP()
     smtp.set_debuglevel(10)
     smtp.connect('smtp.sparkpostmail.com', 587)
@@ -43,6 +42,6 @@ dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-email = PythonOperator(task_id='email_task', python_callable=send_smtp_mail, dag=dag)
+email = PythonOperator(task_id='email_task', python_callable=my_email_func, dag=dag)
 
 email >> dummy_operator >> hello_operator
